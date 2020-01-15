5813d3b689830e074288c62da381bb63637c2b7b
starfish719 <starfish719@gmail.com>
2020-01-15 17:40:55 +0900

    check event type

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 exports.handler = async (event) => {
-    const response = {
+
+    const gitHubBody = JSON.parse(event.body);
+    const eventName = event.headers['X-GitHub-Event'];
+
+    const message = {
+        title: null,
+        url: null,
+        body: null,
+    }
+
+    if (eventName === 'pull_request' && gitHubBody.action === 'opened') {
+        message.title = `#${gitHubBody.pullRequest.number} ${gitHubBody.pullRequest.title}`;
+        message.url = gitHubBody.pullRequest.html_url;
+        message.body = gitHubBody.pullRequest.body;
+    } else if (eventName === 'issues' && gitHubBody.action === 'opened')  {
+        message.title = `#${gitHubBody.issue.number} ${gitHubBody.issue.title}`;
+        message.url = gitHubBody.issue.html_url;
+        message.body = gitHubBody.issue.body;
+    } else if (eventName === 'issue_comment' && gitHubBody.action === 'created')  {
+        message.title = `Comment on #${gitHubBody.issue.number} ${gitHubBody.issue.title}`;
+        message.url = gitHubBody.comment.html_url;
+        message.body = gitHubBody.comment.body;
+    } else if (eventName === 'pull_request_review_comment' && gitHubBody.action === 'created')  {
+        message.title = `Review on #${gitHubBody.pullRequest.number} ${gitHubBody.pullRequest.title}`;
+        message.url = gitHubBody.comment.html_url;
+        message.body = gitHubBody.comment.body;
+    } else {
+        return {
+            statusCode: 200,
+            body: 'No post event',
+        }; 
+    }
+
+    if (!message.body) {
+        return {
+            statusCode: 200,
+            body: 'No message',
+        }; 
+    }
+
+    // TODO POST処理
+    
+    return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Finish'),
     };
-    return response;
 };
